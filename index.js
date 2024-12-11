9c6b3032b6948e84921c9f69ef254374e8bbc2e2
Hiago-Cardoso-Felisberto <hiagocf2003@gmail.com>
2024-12-11 20:31:53 -0300

    correção do erro

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 let Heroi = "Batman";
-let Nivel = "12000";
+let Nivel = 10500;
 
-if(Nivel<1000){
+if(Nivel<=1000){
     Nivel = "Ferro";
 }
-    else if(Nivel>1001 && Nivel<=2000){
+    else if(Nivel>=1001 && Nivel<=2000){
         Nivel = "Bronze";
     }
-        else if(Nivel>2.001 && Nivel<=5.000){
+        else if(Nivel>=2001 && Nivel<=5000){
             Nivel = "Prata"
         }
-            else if(Nivel>5.001 && Nivel<=7.000){
+            else if(Nivel>=5001 && Nivel<=7000){
                 Nivel = "Ouro"
             }
-                else if(Nivel>7.001 && Nivel<=8.000){
+                else if(Nivel>=7001 && Nivel<=8000){
                     Nivel = "Paltina"
                 }
-                    else if(Nivel>8.001 && Nivel<=9.000){
+                    else if(Nivel>=8001 && Nivel<=9000){
                         Nivel = "Ascendente"
                     }
-                        else if(Nivel>9.001 && Nivel<=10.000){
+                        else if(Nivel>=9001 && Nivel<=10000){
                             Nivel = "Imortal"
                         }
                             else{
